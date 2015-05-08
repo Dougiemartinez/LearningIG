e3b701df8c8b3ae550f07f78457c606b11976513
Douglas Martinez <footbalfanmartinez@gmail.com>
2015-05-08 12:15:29 -0700

    video 2

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<?php
+set_time_limit(0);
+ini_set('default_socket_timeout', 300);
+session_start();
+
+//make constants using define
+
+define[client_id, '24abe18a6686415ea47be51d1849117f'];
+define[client_secret, 'c666445cf51a4e4989b8c42a2470d72f'];
+define[redirectURI, 'http://localhost/InstagramProject/index.php'];
+define[ImageDirectory, 'pics/'];
+?>
+
+<!-- CLIENT INFO
+CLIENT ID	24abe18a6686415ea47be51d1849117f
+CLIENT SECRET	c666445cf51a4e4989b8c42a2470d72f
+WEBSITE URL	http://localhost/InstagramProject/index.php -->
