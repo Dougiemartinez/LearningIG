94a33750f6b7cfdb2faace8c598a27f5d2aebe11
Douglas Martinez <footbalfanmartinez@gmail.com>
2015-05-11 13:09:28 -0700

    video 3

--- index.php
+++ index.php
@@ -1,18 +1,36 @@
-
-<?php
-set_time_limit(0);
-ini_set('default_socket_timeout', 300);
-session_start();
-
-//make constants using define
-
-define[client_id, '24abe18a6686415ea47be51d1849117f'];
-define[client_secret, 'c666445cf51a4e4989b8c42a2470d72f'];
-define[redirectURI, 'http://localhost/InstagramProject/index.php'];
-define[ImageDirectory, 'pics/'];
+<?php 
+	//Configuration for PHP server
+	set_time_limit(0);
+	ini_set('default_socket_timeout', 300);
+	//starts session
+	session_start();
+	//Make Constants using define
+	define('client_ID', '025e77c43f93484fb23e475d3530c15c');
+	define('client_Secret', '952be6ecb7d941f89de536e5fdd9d75e');
+	define('redirectURI', 'http://localhost/maxapi/index.php');
+	define('ImageDirectory', 'pics/');
 ?>
 
-<!-- CLIENT INFO
-CLIENT ID	24abe18a6686415ea47be51d1849117f
-CLIENT SECRET	c666445cf51a4e4989b8c42a2470d72f
-WEBSITE URL	http://localhost/InstagramProject/index.php -->
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="">
+	<meta name="viewport" content="width098, intitial-scale = 1">
+	<title>InstApi</title>
+	<meta rel="stylesheet" href="css/style.css">
+	<meta rel="author" href="humans.txt">
+</head>
+<body>
+	<!-- Creating a login for people to go and give approval for our web app to access their Instagram Account
+		 After getting aprroval we are now going to have the information so that we can play with it.
+	 -->
+	<a href="https:api.instagram/oauth/authorize/?client_id= <?php echo client_ID; ?> &redirect_uri= <?php echo redirectURI; ?> &response_type=code">LOGIN</a>
+	<script src="js/main.js"></script>
+</body>
+</html>
+
+CLIENT ID	ebd393ddcaba4500a056cfa0ce5b944d
+CLIENT SECRET	50f0ba8f32b0494588f35a13cdf7bd7b
+WEBSITE URL	http://localhost/instagrampage/index.php
+REDIRECT URI	http://localhost/instagrampage/index.php
