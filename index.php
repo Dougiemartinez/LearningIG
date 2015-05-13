0a541c55f19312fb08c28a8a78a766f1ae2bcd48
Douglas Martinez <footbalfanmartinez@gmail.com>
2015-05-13 12:02:50 -0700

    video 5

--- index.php
+++ index.php
@@ -9,24 +9,32 @@
 	define('clientSecret', '3607ef7b33fb43419b910e82e2bb4260');
 	define('redirectURI', 'http://localhost/LearningIG/index.php');
 	define('ImageDirectory', 'pics/');
+	if (isset($_GET['code'])) {
+		$code = ($_GET['code']);
+		$url = 'https://api.instagram.com/oauth/access_token';
+		$access_token_settings = array('client_id' => clientID,
+									   	'client_secret' => clientSecret,
+										'grant_type' => 'authorization_code',
+										'redirect_uri' => redirectURI, 
+										'code' => $code);
+	}
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta name="description" content="">
-	<meta name="viewport" content="width098, intitial-scale = 1">
-	<title>InstApi</title>
-	<meta rel="stylesheet" href="css/style.css">
-	<meta rel="author" href="humans.txt">
-</head>
-<body>
-	<!-- Creating a login for people to go and give approval for our web app to access their Instagram Account
-		 After getting aprroval we are now going to have the information so that we can play with it.
-	 -->
-	<a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code">Login to Instagram</a></li>
-	<script src="js/main.js"></script>
-</body>
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="">
+		<meta name="viewport" content="width098, intitial-scale = 1">
+		<title>InstApi</title>
+		<meta rel="stylesheet" href="css/style.css">
+		<meta rel="author" href="humans.txt">
+	</head>
+	<body>
+		<!-- Creating a login for people to go and give approval for our web app to access their Instagram Account
+			 After getting aprroval we are now going to have the information so that we can play with it.
+		 -->
+		<a href="https:api.instagram.com/oauth/authorize/?clientID = <?php echo clientID; ?> & redirectURI = <?php echo redirectURI; ?> & response_type=code">LOGIN TO INSTAGRAM</a>
+		<script src="js/main.js"></script>
+	</body>
 </html>
-
