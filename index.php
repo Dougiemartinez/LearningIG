a410d057e3e666545cf40ad74bff05b1e5c8d83c
Douglas Martinez <footbalfanmartinez@gmail.com>
2015-05-14 16:46:42 -0700

    video 7

--- index.php
+++ index.php
@@ -9,15 +9,27 @@
 	define('clientSecret', 'c35a94b5e3d64f0f9677b63c72a99532');
 	define('redirectURI', 'http://localhost/douglasAPI/index.php');
 	define('ImageDirectory', 'pics/');
-	// if (isset($_GET['code'])) {
-	// 	$code = ($_GET['code']);
-	// 	$url = 'https://api.instagram.com/oauth/access_token';
-		// $access_token_settings = array('client_id' => clientID,
-		// 							   	'client_secret' => clientSecret,
-		// 								'grant_type' => 'authorization_code',
-		// 								'redirect_uri' => redirectURI, 
-		// 								'code' => $code);
-	//}
+	if (isset($_GET['code'])) {
+		$code = ($_GET['code']);
+		$url = 'https://api.instagram.com/oauth/access_token';
+		$access_token_settings = array('client_id' => clientID,
+									   	'client_secret' => clientSecret,
+										'grant_type' => 'authorization_code',
+										'redirect_uri' => redirectURI, 
+										'code' => $code);
+		//cURL is a library that calls to other API's
+		//setting curl session and insert $url
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_POST, true);
+		//sets the POSTFIELDS to the array
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $access_token_settings);
+		//sets equal to 1
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		//verifies that the curl is really there
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+	}
+	$result = curl_exec($curl);
+	curl_close();
 ?>
 
 <!DOCTYPE html>
@@ -31,14 +43,11 @@
 		<meta rel="author" href="humans.txt"> -->
 	</head>
 	<body>
-		
 		<h2><center>Welcome<center></h2> 
 		<div><a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code"><h1><center>LOGIN TO INSTAGRAM</center></h1></a></div>
 		<script src="js/main.js"></script>
 		<link rel="stylesheet" type="text/css" href="main.css">
 		<body background="images../bgimage2.jpg">
 		<!-- <body background="images../bgimage2.jpg"> -->
-
-        
 	</body>
 </html>
