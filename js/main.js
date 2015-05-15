c2c90d249981d0e70d1a58764aa5f95646102b3c
Douglas Martinez <footbalfanmartinez@gmail.com>
2015-05-14 17:06:02 -0700

    video 9

---
+++ js/main.js
@@ -0,0 +1,12 @@
+define('clientID', 'fa4f7acd50714993b75b95ab5e3745fb');
+	define('clientSecret', 'c35a94b5e3d64f0f9677b63c72a99532');
+	define('redirectURI', 'http://localhost/douglasAPI/index.php');
+	define('ImageDirectory', 'pics/');
+	<body>
+		<h2><center>Welcome<center></h2> 
+		<div><a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code"><h1><center>LOGIN TO INSTAGRAM</center></h1></a></div>
+		<script src="js/main.js"></script>
+		<link rel="stylesheet" type="text/css" href="main.css">
+		<body background="images../bgimage2.jpg">
+		<!-- <body background="images../bgimage2.jpg"> -->
+	</body>
